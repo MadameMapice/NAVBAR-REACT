aef221cfe7a2ff8b30b976520ed09071c2e6b0a6
Pia <mariapianavach@gmail.com>
2021-11-25 11:24:15 +0100

    commit

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 class Navbar extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={ouvert:true}
+        this.clickBurger=this.clickBurger.bind(this)
+    }
 
+clickBurger(){
+    this.setState({ouvert:!this.state.ouvert})
+}
     render (){
+        let maClasse=""
+        if(this.state.ouvert==true){
+            maClasse="menu flex ouvert"
+        }
+        else{
+            maClasse="menu flex"
+        }
         return <nav className="flex">
                     <h1>NAVBAR</h1>
-
-                <ul className="menu flex">
+                    
+<div className="burger" onClick={this.clickBurger}>
+                    <i className="fas fa-bars"></i>
+                </div>
+                
+                <ul className={maClasse}>
                     <li><a href="#">lien 1</a></li>
                     <li><a href="#">lien 2</a></li>
                     <li><a href="#">lien 3</a></li>
@@ -14,9 +33,7 @@ class Navbar extends React.Component {
                     <li><a href="#">lien 7</a></li>
                 </ul>
 
-                <div className="burger">
-                    <i class="fas fa-bars"></i>
-                </div>
+                
 
                 </nav> 
     }
